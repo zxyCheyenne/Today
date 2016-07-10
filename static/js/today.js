d0101218b3047fe26c5b2eaca7453b84e2be4031
Sherry <sherryyao2015@gmail.com>
2016-07-11 00:18:18 +0800

    update home, follow, search, profile

--- static/js/today.js
+++ static/js/today.js
@@ -1,8 +1,25 @@
+/*
 function setPageHeight() {
 	// load page with proper height
 	$(body).css("min-height", document.body.scrollHeight);
 }
+ */
+
+function hideSide() {
+	weight = $("#nav-mobile").css("width");
+	$("#nav-mobile").css("transform", "translateX(-" + weight + ")");
+	$("header").css("padding-left", "0px");
+	$("main").css("padding-left", "0px");
+}
+
+function showSide() {
+	$("#nav-mobile").css("transform", "translateX(0%)");
+	$("header").css("padding-left", "240px");
+	$("main").css("padding-left", "200px");
+}
 
 window.onload = function() {
-	setPageHeight();
+	$("#side-nav-show").onclick = function() {
+		$("#nav-mobile").css("transform", "translateX(0%)");
+	};
 }
